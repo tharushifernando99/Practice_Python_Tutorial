56e8f110738af3bda35e90be1856d24d2c4c2b0d
neurafinal05 <neurafinalproject@gmail.com>
2025-01-29 11:52:15 +0530

    Create Q1.py
    Question 1

---
+++ Q1.py
@@ -0,0 +1,13 @@
+#*************Add two numbers from user inputs******************
+
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+
+# Total = number1 + number2
+# print(Total)
+
+num1 = 30
+num2 = 50
+sum = num1 + num2
+print(sum)
